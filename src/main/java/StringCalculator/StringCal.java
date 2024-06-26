47fbaa6f00d3fe047afc4579a04b0c973aa35606
Yadavendra Sakharkar <prajwal1619@gmail.com>
2024-06-26 23:22:26 +0530

    Allow custom delims

--- src/main/java/StringCalculator/StringCal.java
+++ src/main/java/StringCalculator/StringCal.java
@@ -1,15 +1,41 @@
 package StringCalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCal {
     public static int add(String numbers){
         if(numbers.isEmpty())return 0;
-        String[] nums = numbers.split("[,\n" +
-                "]");
+        String[] nums = customSplit(numbers);
         int size = nums.length;
         return calSum(nums, size);
     }
+    //below function is added to support custom delimiter apart from comma and newline
+    private static String[] customSplit(String numbers) {
+        if(numbers.isEmpty())return new String[0];
+        else if(hasCustomDelimiter(numbers)){
+            return splitCustomDelimiter(numbers);
+        }
+        else return splitCommaOrNewline(numbers);
+    }
 
-    public static int calSum(String[] nums, int size){
+    private static String[] splitCustomDelimiter(String numbers) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+        matcher.matches();
+        String delim = matcher.group(1);
+        String newNumbers = matcher.group(2);
+        return newNumbers.split(Pattern.quote(delim));
+    }
+    //below method is to check for custom delimiter
+    private static boolean hasCustomDelimiter(String numbers) {
+        return numbers.startsWith("//");
+    }
+
+    private static String[] splitCommaOrNewline(String numbers) {
+        return numbers.split("[\n" +
+                ",]");
+    }
+    private static int calSum(String[] nums, int size){
         int sum = 0;
         if(size==0)return 0;
         for(int i=0;i<size;i++){
--- src/test/java/StringCalTest.java
+++ src/test/java/StringCalTest.java
@@ -6,17 +6,17 @@ import static org.junit.Assert.*;
 
 public class StringCalTest {
     //TASK 1
-    //test for empty string
+    //TODO: test for empty string
     @Test
     public void returnZeroOnEmptyInput(){
         assertEquals(0, add(""));
     }
-    //test for string with two numbers separated by comma
+    //TODO: test for string with two numbers separated by comma
     @Test
     public void returnSumOfTwoNumberCommaSeparated(){
         assertEquals(5, add("2,3"));
     }
-    //test for string with a single number
+    //TODO: test for string with a single number
     //for single number it should return the same number
     @Test
     public void returnSameNumberForSingleNumber(){
@@ -24,7 +24,7 @@ public class StringCalTest {
     }
 
     //TASK2
-    //test for multiple strings separated by comma
+    //TODO:test for multiple strings separated by comma
     //output should be sum of all
     @Test
     public void returnSumOfAllNumbers(){
@@ -32,9 +32,18 @@ public class StringCalTest {
     }
 
     //TASK3
-    //Allow the Add method to handle new lines between numbers instead of commas
+    //TODO:Allow the Add method to handle new lines between numbers instead of commas
     @Test
     public void allowNewlineAndCommaAsDelim(){
         assertEquals(6,add("1\n2,3"));
+    }
+
+    //TASK 4
+    //TODO: Support different delimiters
+    //allow custom delimiters
+    @Test
+    public void allowCustomDelims(){
+        assertEquals(3,add("//;\n1;2"));
+        assertEquals(6,add("//;\n1;2;3"));
     }
 }
