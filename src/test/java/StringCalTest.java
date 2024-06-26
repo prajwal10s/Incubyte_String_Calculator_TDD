337230f1eab4a47f82add931126792d52cc444ed
Yadavendra Sakharkar <prajwal1619@gmail.com>
2024-06-26 22:34:54 +0530

    Adding testcases for single digit and sum of two digits

--- src/test/java/StringCalTest.java
+++ src/test/java/StringCalTest.java
@@ -5,8 +5,21 @@ import static org.junit.Assert.*;
 
 
 public class StringCalTest {
+
+    //test for empty string
     @Test
     public void returnZeroOnEmptyInput(){
         assertEquals(0, add(""));
+    }
+    //test for string with two numbers separated by comma
+    @Test
+    public void returnSumOfTwoNumberCommaSeparated(){
+        assertEquals(5, add("2,3"));
+    }
+    //test for string with a single number
+    //for single number it should return the same number
+    @Test
+    public void returnSameNumberForSingleNumber(){
+        assertEquals(2,add("2"));
     }
 }
